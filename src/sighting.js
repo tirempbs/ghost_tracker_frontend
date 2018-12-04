b60ffdd86af9d884782fc677c8a0011074893e29
brewchetta <chett.tiller@gmail.com>
2018-12-04 17:10:54 -0500

    Added basic functionality for sighting class

---
+++ src/sighting.js
@@ -0,0 +1,14 @@
+class Sighting {
+  constructor(body) {
+    this.id = body.id
+    this.entity = body.entity
+    this.lat = body.lat
+    this.long = body.long
+    this.image = body.image
+    this.description = body.description
+    this.userID = body.user.id
+    allSightings.push(this)
+  }
+}
+
+allSightings = []
