6ba3554ef58041b3eaaa3a40adbfb10c7310afa0
tirempbs <tirem@fastmail.com>
2018-12-05 11:22:12 -0500

    rendering seeds on page load

--- src/leaflet.js
+++ src/leaflet.js
@@ -1,7 +1,9 @@
+let nycMap
+
 document.addEventListener('DOMContentLoaded', () => {
 
   //initialize map, set coordinates and zoom
-  const nycMap = L.map('nyc-map').setView([40.743, -74], 13);
+  nycMap = L.map('nyc-map').setView([40.743, -74], 13);
 
   //add tile layer to map
   // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -14,31 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
     	maxZoom: 16
   }).addTo(nycMap);
 
+  
+
 
   //flatiron school marker
-  var flatiron = L.marker([40.7052529, -74.0140703]).addTo(nycMap);
+  // var flatiron = L.marker([40.7052529, -74.0140703]).addTo(nycMap);
 
   //stay puft marshmallow man marker
-  var stayPuft = L.marker([40.767382, -73.981713]).addTo(nycMap);
+  // var stayPuft = L.marker([40.767382, -73.981713]).addTo(nycMap);
 
   //500m circle around flatiron school
-  var touristAvoidanceZone = L.circle([40.757, -73.986], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500 //in meters
-  }).addTo(nycMap);
+  // var touristAvoidanceZone = L.circle([40.757, -73.986], {
+  //   color: 'red',
+  //   fillColor: '#f03',
+  //   fillOpacity: 0.5,
+  //   radius: 500 //in meters
+  // }).addTo(nycMap);
 
   //popups (.openPopup has popup open by default)
 
-  flatiron.bindPopup("<b>Hello</b> from Flatiron School!").openPopup();
+  // flatiron.bindPopup("<b>Hello</b> from Flatiron School!").openPopup();
 
-  touristAvoidanceZone.bindPopup("Tourist Avoidance Zone")
+  // touristAvoidanceZone.bindPopup("Tourist Avoidance Zone")
 
-  stayPuft.bindPopup(`<img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Stay-puft-marshmallow-man.jpg/320px-Stay-puft-marshmallow-man.jpg" /> I tried to think of the most harmless thing. Something I loved from my childhood, something that could never, ever possibly destroy us: Mr. Stay-Puft`)
+  // stayPuft.bindPopup(`<img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Stay-puft-marshmallow-man.jpg/320px-Stay-puft-marshmallow-man.jpg" /> I tried to think of the most harmless thing. Something I loved from my childhood, something that could never, ever possibly destroy us: Mr. Stay-Puft`)
 
   //listener and callback for map click; lat and long can be retreived from this function
-
   function onMapClick(event) {
     console.log("You clicked the map at " + event.latlng);
     //event.latlng.lat = latitude as integer
@@ -46,5 +49,24 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   nycMap.on('click', onMapClick);
+
+  //pull all the seed sightings into allSightings array
+  loadAllSightings()
+
+  //iterate through allSightings and create and render pins
+
+  renderSightings = () => {
+    allSightings.forEach( s => {
+      let newS = L.marker([parseFloat(s.lat), parseFloat(s.long)]).addTo(nycMap);
+
+      newS.bindPopup(`
+        <h3>${s.entity}</h3>
+        <img src=${s.image} width=300>
+        ${s.description}
+      `)
+    })
+  }
+
+  renderSightings()
 
 })
