519ebdfa6c2f7fe6cf9659a0d1bedc95631f4c28
tirempbs <tirem@fastmail.com>
2018-12-07 10:55:35 -0500

    added alien monster type

--- src/giphy.js
+++
@@ -1,9 +0,0 @@
-const searchButton = document.querySelector('input[name=giphy-search]')
-
-const searchGiphy = (string) => {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${string}&api_key=3z8UvhNbY64J20z8qW6UICnAvJQgEZbI&limit=5`)
-        .then(resp => resp.json())
-        .then(json => console.log(json))
-}
-
-searchButton.addEventListener('submit', searchGiphy(imageSearch))
